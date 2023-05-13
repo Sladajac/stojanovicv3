355bad504de1206992277be2d93db1371796c7aa
Sladajac <118850174+Sladajac@users.noreply.github.com>
2023-05-13 12:13:39 +0200

    Add files via upload

---
+++ dist/js/app.bundle.js
@@ -0,0 +1,9 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){"use strict";Object.defineProperty(exports,"__esModule",{value:!0});const Nav_1=require("./components/Nav"),btnShowMenu=document.querySelector("#mobileNavBtn");
+//!Show nav
+btnShowMenu.addEventListener("click",Nav_1.prikaziMeni);
+//!Godine brojac i broj klieanata
+let valueDisplays=document.querySelectorAll(".brojBrojaca"),interval=4e3;valueDisplays.forEach(e=>{let t=0,a=parseInt(e.getAttribute("data-val")),n=Math.floor(interval/a),r=setInterval(function(){t+=1,e.textContent=t,t==a&&clearInterval(r)},n)}),
+//!Animacije na load stranice
+window.addEventListener("load",()=>{const e=document.querySelector("#bannerTitle");e.classList.add("animate__animated"),e.classList.add("animate__fadeInDown")})},{"./components/Nav":2}],2:[function(require,module,exports){"use strict";function prikaziMeni(){const e=document.querySelector("#activeMobileNavSec");e.style.width="100%",document.querySelector("#closeMobileNav").addEventListener("click",()=>{e.style.width="0"})}Object.defineProperty(exports,"__esModule",{value:!0}),exports.prikaziMeni=void 0,exports.prikaziMeni=prikaziMeni},{}]},{},[1]);
+//# sourceMappingURL=app.bundle.js.map
+//# sourceMappingURL=app.bundle.js.map
